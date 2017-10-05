5226313948ed2cc8c743bf0dc692102ca97b8bae
Rijwan <rijwan.matin@iproperty.com>
2017-10-05 16:40:14 +0800

    adding POST method

--- routes/cvApiRoutes.ts
+++ routes/cvApiRoutes.ts
@@ -9,20 +9,19 @@ module.exports = function(app: any) {
     })
   })
 
-  app.get('/cvlist', (req: any, res: any) =>{
-    request('http://localhost:3000/cv', function (error:any, response:any, body:any) {
+  app.get('/list', (req: any, res: any) =>{
+    request.get('http://localhost:3000/cv', function (error:any, response:any, body:any) {
       console.log('error:', error);
       console.log('statusCode:', response && response.statusCode);
       console.log('body:', body);
       res.send(JSON.parse(body))
     })
   })
-  app.post('/cvlist', (req: any, res: any) =>{
-    request.post({url:'http://localhost:3000/cv'}, function optionalCallback(err, httpResponse, body) {
-      if (err) {
-        return console.error('upload failed:', err);
-      }
-      console.log('Upload successful!  Server responded with:', body);
+  app.post('/add', (req: any, res: any) =>{
+    request.post({url:'http://localhost:3000/cv',form:req.body}, function (error:any, response:any, body:any) {
+      console.log('error:', error);
+      console.log('statusCode:', response && response.statusCode);
+      console.log('body:', body);
       res.send(JSON.parse(body))
     })
   })
