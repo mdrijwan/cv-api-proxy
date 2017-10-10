53bce320cf07c2c8c0948b454d1130b4c1748201
Rijwan <rijwan.matin@iproperty.com>
2017-10-10 15:52:59 +0800

    updating routes

--- routes/cvApiRoutes.ts
+++ routes/cvApiRoutes.ts
@@ -41,7 +41,7 @@ module.exports = function(app: any) {
 
     // to edit a specific cv (not being able to update, needs to be fixed)
     app.put('/edit/:cvId', (req: any, res: any) =>{
-      request.put('http://localhost:3000/cv/' + req.params.cvId, function (error:any, response:any, body:any) {
+      request.put('http://localhost:3000/cv/' + req.params.cvId, {form:req.body}, function (error:any, response:any, body:any) {
         console.log('error:', error);
         console.log('statusCode:', response && response.statusCode);
         console.log('body:', body);
